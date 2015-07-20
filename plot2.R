770d5d25a26d603c275b739d27a3edbf40ef92ec
Andrew Crisp <crispa@slhs.org>
2015-07-20 15:01:59 -0600

    Generate plot2, total of PM2.5 in each year in Baltimore City, Maryland.

---
+++ plot2.R
@@ -0,0 +1,32 @@
+################################################################################
+#
+# Author:         Andrew Crisp
+# Filename:   		plot2.R
+# Comments:			  
+#						      This script will:
+#  					      1. Source the loadData.R script to load data.
+#  					      2. Demonstrate with Base graphics the total PM2.5 emissions
+#    				          from all sources for each of the years 1999, 2002, 2005,
+#    				          2008.
+#    				      
+################################################################################
+################################################################################
+### Imports
+################################################################################
+library(dplyr)
+
+################################################################################
+### Load dataset
+################################################################################
+source('loadData.R')
+
+interestingData <- summaryData[which(summaryData$fips == "24510"), c(18,20)]
+
+totalPM25 <- interestingData %>%
+  tbl_df %>%
+  group_by(year) %>%
+  summarise_each(funs(sum))
+
+png("plot2.png", width=480, height=480)
+barplot(height = totalPM25$Emissions, names.arg = totalPM25$year, main = "Total PM2.5 Emissions by Year in Baltimore City, Maryland", xlab = "Year", ylab = "Total PM2.5 Emissions")
+dev.off()
